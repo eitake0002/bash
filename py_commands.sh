fcb25b26e1b37af1896b738d3215a108d69ccdee
root <eiki.e.t.d@gmail.com>
2016-08-18 13:44:40 +0900

    add py_command script

---
+++ py_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# list available pakcages
+pyenv install --list
+
+# install
+pyenv install 2.7.12
+
+# list installed versions. 
+pyenv versions
+
+# Switch version.
+pyenv global 2.7.12
