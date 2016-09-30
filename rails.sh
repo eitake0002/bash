e203f2c4e6e8f0b05bbc5a5283eb8a9c24df55fd
eiki.takeuchi <b-engineer-takeuchi@co.kayac.com>
2016-09-30 17:07:28 +0900

    Fix rails.sh

--- rails.sh
+++ rails.sh
@@ -12,32 +12,32 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # Rails application name
-export app_name=eto-maker
+# export rails_app_name=clorets-ar
 
 #----------------------------------
 # Frequently used directories
 #----------------------------------
-alias app='cd ~/$app_name/'
-alias conts='cd ~/$app_name/app/controllers/'
-alias models='cd ~/$app_name/app/models/'
-alias views='cd ~/$app_name/app/views/'
-alias admin='cd ~/$app_name/app/admin/'
-alias lib='cd ~/$app_name/lib/'
-alias config='cd ~/$app_name/config/'
-alias config-init='cd ~/$app_name/config/initializers'
-alias mig='cd ~/$app_name/db/migrate/'
+alias app='cd ~/$rails_app_name/'
+alias conts='cd ~/$rails_app_name/app/controllers/'
+alias models='cd ~/$rails_app_name/app/models/'
+alias views='cd ~/$rails_app_name/app/views/'
+alias admin='cd ~/$rails_app_name/app/admin/'
+alias lib='cd ~/$rails_app_name/lib/'
+alias config='cd ~/$rails_app_name/config/'
+alias config-init='cd ~/$rails_app_name/config/initializers'
+alias mig='cd ~/$rails_app_name/db/migrate/'
 alias spec='cd ~/autopost/current/spec'
 alias spec-lib='cd ~/autopost/current/spec/lib'
 
 #----------------------------------
 # Frequently used files
 #----------------------------------
-alias vim-routes='vim ~/$app_name/config/routes.rb'
+alias vim-routes='vim ~/$rails_app_name/config/routes.rb'
 alias vim-seed='vim ~/autopost/current/db/seeds.rb'
 
 #----------------------------------
 # Server commands
 #----------------------------------
 alias restart-unicorn='cd /home/ec2-user/autopost/current/tools/unicorn; sh unicorn.sh stop; sh unicorn.sh start'
-alias run-webrick='cd ~/$app_name/; bundle exec rails s -b 0.0.0.0'
-alias run-webrick-bg='cd ~/$app_name/; bundle exec rails s -b 0.0.0.0 &'
+alias run-webrick='cd ~/$rails_app_name/; bundle exec rails s -b 0.0.0.0'
+alias run-webrick-bg='cd ~/$rails_app_name/; bundle exec rails s -b 0.0.0.0 &'
