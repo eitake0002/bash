d357a458f34d30c2b26537456c1e3e41b0589d11
root <eiki.e.t.d@gmail.com>
2016-09-30 09:13:50 +0900

    Edit files

--- linux.sh
+++ linux.sh
@@ -1,88 +1,61 @@
 #!/bin/sh
 
-# Get the aliases and functions
-if [ -f ~/.bashrc ]; then
-	. ~/.bashrc
-fi
+#bashedit-li='vim ~/bash_profile/linux.sh'
 
-##### Prompt setting #####
-host_name=`hostname`
-PS1="${host_name} \W$ "
+#----------------------------------
+# Common settings.
+#----------------------------------
 
-#------------
+# Set prompt name
+PS1="`hostname` \W$ "
+
+# Read .bash_profile
+alias sr='cd ~; source ~/.bash_profile'
+
+#----------------------------------
 # OS settins
-#------------------------
-# User specific environment and startup programs
-PATH=$PATH:$HOME/bin
-export PATH
+#----------------------------------
+# Add ruby command
+export PATH=$PATH:$HOME/bin
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
-##### Rails settings #####
-export RAILS_ENV=development
-export RAILS_APP=/home/ec2-user/app
-export MECAB_PATH=/usr/lib64/libmecab.so.2
-
-##### Prompt setting #####
-host_name=`hostname`
-PS1="${host_name} \W$ "
-
-##### UTILS #####
-
-# bash utils. 
-alias sr='cd ~; source ~/.bash_profile'
-alias bashedit='vim ~/.bash_profile'
-alias bashedit-com='vim ~/bash_profile/common.bash_profile'
-alias bashedit-clo='vim ~/bash_profile/clorets.bash_profile'
-alias bashedit-share='vim ~/bash_profile/share-gacha.bash_profile'
-alias bashedit-ec='vim ~/bash_profile/ec_dev.bash_profile'
-alias bashedit-mac='vim ~/bash_profile/local-mac-pc.bash_profile'
-alias bashedit-py='vim ~/bash_profile/python.bash_profile'
-alias bs='cd ~/bash_profile'
-alias bashcp='cp ~/.bash_profile ~/bash_profile/bash_profile'
-alias ll='ls -la --color=auto'
-alias grep='grep --color'
-alias awk-tools='cd ~/bash_profile/awk_tools'
-
+#----------------------------------
 # ssh
+#----------------------------------
 alias ssh-config='vim ~/.ssh/config'
 
-# network. 
+#----------------------------------
+# Network 
+#----------------------------------
 alias global='curl inet-ip.info'
 
+#----------------------------------
+# Process
+#----------------------------------
+
+#----------------------------------
+# Storage
+#----------------------------------
+
+#----------------------------------
+# Memory
+# ----------------------------------
 # sleep count. 
-function count(){
-  while [ 1 == 1 ]
-  do
-    date
-    sleep 1
-  done
-}
 
-# search. 
+#----------------------------------
+# Search
+# ----------------------------------
+
+# Search file/diretory name.
+function fi(){
+  find / -name $1
+}
+# Search word in multiple files
 function fin() {
-  find ./ -type f -print | xargs grep $1
+  find ./ -type f -print | xargs grep $2
 }
 
-# Ruby settings. 
-PATH=$PATH:$HOME/bin
-export PATH
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-##### Rails settings #####
-export RAILS_ENV=development
-export RAILS_APP=/home/ec2-user/autopost/current
-export MECAB_PATH=/usr/lib64/libmecab.so.2
-
-##### MySQL #####
-alias show-dbs="mysql -u root -e 'show databases;'"
-function showtables(){
-  mysql -u root $1 -e 'show tables;'
-}
-function mysql-version(){
-  mysql -u root -D mysql -e "SELECT version()"
-}
 ##### Git #####
 git config --global color.ui true
 
--- local-mac-pc.bash_profile
+++ local-mac-pc.bash_profile
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 # UTILS
 alias ll='ls -la'
