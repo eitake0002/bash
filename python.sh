7f4a059036b686e5556c2ab0a1aa24d9bebf60b9
root <eiki.e.t.d@gmail.com>
2016-09-30 20:40:18 +0900

    edit python.sh

--- python.sh
+++ python.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-##### Django #####
-alias jango='cd /home/ec2-user/py/'
-alias jango-version='python -c "import django; print(django.get_version())"'
-alias runjango='cd /home/ec2-user/py/; python manage.py runserver 0.0.0.0:8000 &'
-alias debugsql='cd /home/ec2-user/py/; python manage.py debugsqlshell'
-alias create-super-user='python manage.py createsuperuser'
-alias api='cd /home/ec2-user/py/mysite/api;'
-alias mysite='cd /home/ec2-user/py/mysite/mysite/'
-alias mig='cd /home/ec2-user/py/autopost/migrate/'
+#------------------------------------
+# pyenv
+#------------------------------------
+alias py-ver='pyenv versions'
 
-##### Jupyter #####
-alias ju='cd /home/ec2-user/py/autopost/jupyter'
-alias runju='cd /home/ec2-user/py/autopost/jupyter; jupyter notebook --debug > jupyter_log.log 2>&1 &'
+# Install python.
+# ex: pyins 3.4.1
+function py-ins(){
+  pyenv install $1
+}
+
+# Pyenv global setting.
+# ex: py-glo 3.4.1
+function py-glo(){
+  pyenv global $1
+}
+
+#------------------------------------
+# One liner
+#------------------------------------
+function py-one(){
+  python -c "print '$1'"
+}
+
+#------------------------------------
+# Jupyter
+#------------------------------------
+alias ju='cd ~/py/autopost/jupyter'
+alias runju='cd ~/py/autopost/jupyter; jupyter notebook --debug > jupyter_log.log 2>&1 &'
 alias killju='pgrep jupyter | xargs kill -9'
-
-#### Python ####
-alias auto='cd /home/ec2-user/py/autopost'
-alias com='cd /home/ec2-user/py/autopost/management/commands'
-#alias tools='cd /home/ec2-user/py/tools'
-alias rds='cd /home/ec2-user/py/tools/mysql'
-
