28d67927f8a5a797d49eb420ec67acb2c5ebcf73
eitake0002 <eiki.e.t.d@gmail.com>
2016-10-03 09:15:18 +0000

    add setup file

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo '##### effective work #####' >> ~/.bash_profile
-echo '. ./bash_profile/linux.sh'  >> ~/.bash_profile
-echo '. ./bash_profile/mysql.sh'  >> ~/.bash_profile
-echo '. ./bash_profile/rails.sh'  >> ~/.bash_profile
