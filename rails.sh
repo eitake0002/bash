c297cefbc90a2b87ce90b1251082ce88f0eb2b26
eitake0002 <eiki.e.t.d@gmail.com>
2016-10-06 02:58:37 +0000

    Edit rails.sh

--- rails.sh
+++ rails.sh
@@ -17,23 +17,50 @@ eval "$(rbenv init -)"
 #----------------------------------
 # Frequently used directories
 #----------------------------------
+
+# app
 alias app='cd ~/$rails_app_name/'
+
+# controllers
 alias conts='cd ~/$rails_app_name/app/controllers/'
+
+# controllers/concerns
 alias conc='cd ~/$rails_app_name/app/controllers/concerns'
+
+# models
 alias models='cd ~/$rails_app_name/app/models/'
+
+# views
 alias views='cd ~/$rails_app_name/app/views/'
+
+# active admin app/admin
 alias admin='cd ~/$rails_app_name/app/admin/'
+
+# lib/original libraries.
 alias lib='cd ~/$rails_app_name/lib/'
+
+# config
 alias config='cd ~/$rails_app_name/config/'
 alias config-init='cd ~/$rails_app_name/config/initializers'
+
+# migration
 alias mig='cd ~/$rails_app_name/db/migrate/'
-alias spec='cd ~/autopost/current/spec'
-alias spec-lib='cd ~/autopost/current/spec/lib'
+
+# rspec
+alias spec='cd ~/$rails_app_name/spec'
+alias spec-lib='cd ~/$rails_app_name/spec/lib'
+
+# public
+alias pub='cd ~/$rails_app_name/public/'
 
 #----------------------------------
 # Frequently used files
 #----------------------------------
+
+# config/routes.rb
 alias vim-routes='vim ~/$rails_app_name/config/routes.rb'
+
+# db/seeds.rb
 alias vim-seed='vim ~/$rails_app_name/db/seeds.rb'
 
 #----------------------------------
