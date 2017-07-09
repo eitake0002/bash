b5d2fda8290f06eb7708b81cd507d69c219c933a
eiki takeuchi <eiki.e.t.d@gmail.com>
2017-07-09 15:49:49 +0900

    add add-epel-repository-centos7()

--- yum.sh
+++ yum.sh
@@ -1,7 +1,14 @@
 
 # Description:
 #   Add epel repository on CentOS6
-function add-epel-repo()
+function add-epel-repo-centos6()
 {
   rpm -ivh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+}
+
+# Description:
+#   Add epel repository on CnetOS7
+function add-epel-repo-centos7()
+{
+  yum install epel-release
 }
